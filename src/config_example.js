a99a9e4bb96da471aa3da4a5daf170d84894965c
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-11-03 18:34:21 +0300

    Update config_example

--- src/config_example.js
+++ src/config_example.js
@@ -1,7 +1,9 @@
 
 const config = {
   wsEndpoint: process.env.wsEndpoint || 'wss://unique.usetech.com',
-  adminSeed: "//Alice"
+  ownerSeed: "//Alice"
+  collectionDataSize: 256,
+  collectionId: 13
 };
 
 module.exports = config;
