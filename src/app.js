d076df0a5de26a4e03a5b6d77bdc7f568998deb2
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-11-06 18:36:09 +0300

    Increase request limits

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const config = require('./config');
 const routes = require('./routes/routes');
 
 const port = 3003;
@@ -9,8 +8,8 @@ const app = express();
 
   // Configure content security
 const allowedOrigins = ['http://localhost:3000', 'https://uniqueapps.usetech.com'];
-app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-app.use(bodyParser.json({ limit: '5mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true, parameterLimit: 5000 }));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
